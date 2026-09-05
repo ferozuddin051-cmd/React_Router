ac272e900fb20528364c4d9fb5714ec65c70a2eb
ferozuddin051-cmd <ferozuddin051@gmail.com>
2026-09-05 11:26:01 +0500

    Rendering Nested Elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,46 @@
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Movie } from "./pages/Movie";
 import { Contact } from "./pages/Contact";
+import AppLayout from "./components/layout/AppLayout";
 
 const App = () => {
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Home/>
-  //   },
-  //   {
-  //     path:"/about",
-  //     element: <About/>
-  //   },
-  //   {
-  //     path:"/movie",
-  //     element: <Movie/>
-  //   },
-  //   {
-  //     path:"/contact",
-  //     element: <Contact/>
-  //   },
-  // ]);
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route>
-        <Route path= "/" element= {<Home/>} />
-        <Route path= "/about" element= {<About/>} />
-        <Route path= "/movie" element= {<Movie/>} />
-        <Route path= "/contact" element= {<Contact/>} />
-      </Route>
-    )
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<AppLayout />,
+      children: [
+       {
+      path: "/",
+      element: <Home/>
+    },
+    {
+      path:"/about",
+      element: <About/>
+    },
+    {
+      path:"/movie",
+      element: <Movie/>
+    },
+    {
+      path:"/contact",
+      element: <Contact/>
+    } 
+      ]
+    },
+   
+  ]);
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route>
+  //       <Route path= "/" element= {<Home/>} />
+  //       <Route path= "/about" element= {<About/>} />
+  //       <Route path= "/movie" element= {<Movie/>} />
+  //       <Route path= "/contact" element= {<Contact/>} />
+  //     </Route>
+  //   )
+  // )
 
   return <RouterProvider router={router}/>;
 };
---
+++ src/components/layout/AppLayout.jsx
@@ -0,0 +1,16 @@
+// import { Home } from "../../pages/Home";
+import { Outlet } from "react-router-dom";
+import { Footer } from "./Footer"
+import { Header } from "./Header"
+
+const AppLayout = () => {
+    return (
+    <>
+     <Header />
+     <Outlet />
+     <Footer />
+    </>
+    );
+};
+
+export default AppLayout;
